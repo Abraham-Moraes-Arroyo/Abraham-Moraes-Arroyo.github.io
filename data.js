121f43b8596b174df363c49285f304a63488d87e
Abraham Morales Arroyo <amoral59@uic.edu>
2021-04-08 01:03:39 +0000

    I have added the card funciton and I have been able to populate some of the requirements that were asked from the assignment. I am having issues with the screenshot and the links to the approriate pages. I do plan to continue working on this

---
+++ data.js
@@ -0,0 +1,22 @@
+let projects = [
+
+  {"name": "Tracker program ",
+  "tech": "For this project I used HTML,CSS, and JS. The main purpose of the project was to make a program that tracked the user's position and have the long and lat of the user's position be printed on the screen every time the user moves position",
+  "url_to_page": "https://abraham-moraes-arroyo.github.io/IT202-Spring2021-activities/locationAwareMap.html",
+  "url_to_repo": "https://github.com/Abraham-Moraes-Arroyo/IT202-Spring2021-activities/blob/main/locationAwareMap.html",
+  "date": "4/31/2021"},
+  
+  {"name": "Event Handler Basics",
+  "tech": "For this project I used html, and the main purpose of this was getting used upon event handlers, which allow the user to interact with the page more. An example of this is when a user with his or her mouse goes over an image and it zooms in. Or when a user clicks on a quote and their action causes them to be sent to a differnt page",
+  "url_to_page": "https://abraham-moraes-arroyo.github.io/IT202-Spring2021-activities/event_handler_basics.html",
+  "url_to_repo": "https://github.com/Abraham-Moraes-Arroyo/IT202-Spring2021-activities/blob/main/event_handler_basics.html",
+  "date": "Summer 2021"},
+  
+  {"name": "Land collection",
+  "tech": "For this project I used html and javascript. I also had to use bootstrap. The main purpose of the project was to make an app that checked land collections based on the user's input where the user's input. The results will be printed out in cards and the placed on the map",
+  "url_to_page": "https://abraham-moraes-arroyo.github.io/IT202-Spring2021-project1/index.html",
+  "url_to_repo": "https://github.com/Abraham-Moraes-Arroyo/IT202-Spring2021-project1",
+  "date": "2/16/2021"}
+
+
+];
